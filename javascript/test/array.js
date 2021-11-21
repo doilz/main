e8e12d10d46d209a15f39da08eec94f9464ec205
doilz <dotteda@gmail.com>
2021-11-22 08:56:31 +0900

    af

---
+++ javascript/test/array.js
@@ -0,0 +1,7 @@
+var a = ["a", "e", "d", "c", "b"];
+
+a.forEach((a, b, c) => {
+console.log("value : ", a);
+console.log("index : ", b);
+console.log("arraystring : ", c);
+});
--- nodejs/nodejs3/createServer/client.js
+++ nodejs/nodejs3/createServer/client.js
@@ -12,14 +12,15 @@ var options = {
 
 var callback = (response) => {
     var body = "";
-    response.on('data', function(data) {
+    response.on('data', function(data) {     //데이타가 들어오는 이벤트가 됐을때 , 데이타 (html)를 갖고와서 바디에다 데이타를 넣는다.
         body += data;
     }); //나중에 배울 이벤트
 
-    response.on('end', () => {
+    response.on('end', () => {   // 이벤트가 끝날때 콘솔에 바디를 찍어.
         console.log(body);
     }); //end 라는 이벤트 발생
 };
 
 var req = http.request(options, callback);
 req.end();
+
