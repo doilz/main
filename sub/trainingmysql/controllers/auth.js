2153cd43c0be7a8acfe549b3be0e2df7dba7c8f7
doilz <dotteda@gmail.com>
2021-12-06 20:33:31 +0900

    aa

--- sub/trainingmysql/controllers/auth.js
+++ sub/trainingmysql/controllers/auth.js
@@ -1,4 +1,15 @@
+const mysql = require("mysql"); // db 호출
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form submitted");
+    
+    const { userid, }
 }
