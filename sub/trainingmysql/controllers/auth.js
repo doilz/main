d7fa6662429d51eccdf610def45f985cb8a69c37
doilz <dotteda@gmail.com>
2021-12-06 20:03:06 +0900

    lots of updates

--- sub/trainingmysql/controllers/auth.js
+++ sub/trainingmysql/controllers/auth.js
@@ -1,4 +1,57 @@
+const mysql = require("mysql"); // db 호출
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+// exports.register = (req, res) => {
+//     console.log(req.body);
+//     // res.send("Form submitted");
+//     // const name = req.body.name;
+//     // const email = req.body.email;
+//     // const password = req.body.password;
+//     // const passwordConfirm = req.body.passwordConfirm;
+//     // const { userid, email, userpwd, passwordConfirm  = req.body };
+
+//     // });
+//     // users에서 email컬럼을 선택
+// }
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form submitted");
+    
+    const { userid, email, userpwd, pwdConfirm } = req.body;
+
+    db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {  // ? = [email]
+            if(err) throw err;
+
+            console.log(err);
+
+            if(results.length > 0) {
+                return res.render('/register', {
+                    message: 'That email has already in use.'
+                })
+            } else if( userpwd !== pwdConfirm ) {
+                return res.render('register', {
+                    message: 'Passwords do not match'
+                })
+            }
+    
+            let hashPassword = bcrypt.hash(userpwd, 8);
+            console.log(hashPassword);
+
+            db.query('INSERT INTO users SET?', {userid: userid, email: email, userpwd: hashPassword}, (err, results) => {
+                if(err) throw err;
+
+                res.render('register', {
+                    message: 'user Registered'
+                })
+            });
+    });
 }
--- sub/trainingmysql/routes/pages.js
+++ sub/trainingmysql/routes/pages.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
+const mysql = require("mysql");
 const router = express.Router();
+
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -9,5 +18,34 @@ router.get('/', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('register');
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+router.post('/auth/register', (req, res) => {
+   
+});
+// router.post('/auth/register', (req, res) => {
+//     // const param = [ req.body.userid, req.body.userpwd,req.body.pwdConfirm, req.body.email ];
+  
+//     // db.query('INSERT INTO users (userid, userpwd, pwdConfirm, email) VALUES (?,?,?,?)', param, (err, row) => {
+//     //     if(err) throw err
+
+//     // });
+
+//     // db.query(`SELECT * FROM users WHERE userid = '${req.body.userid}'`, (err, result) => {
+//     //     if(err) throw err;
+
+        
+
+//     //   });
+      
+
+//     // res.end();
+// })
+
+
 
 module.exports = router;
